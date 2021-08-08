f7fd31c416da1a546e9a03d53aa75bd020e69068
OlyaArta <artanika8@gmail.com>
2021-08-08 14:36:42 +0300

    fix some undefined

--- task3.js
+++ task3.js
@@ -10,7 +10,7 @@ const randomIntegerFromInterval = (min, max) => {
         const canProcess = Math.random() > 0.3;
     
         if (canProcess) {
-          ok(transaction.id, delay);
+          ok([transaction.id, delay]);
         } else {
           error(transaction.id);
         }
@@ -18,7 +18,7 @@ const randomIntegerFromInterval = (min, max) => {
    });
   };
   
-  const logSuccess = (id, time) => {
+  const logSuccess = ([id, time]) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
   
